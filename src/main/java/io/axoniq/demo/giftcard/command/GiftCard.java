f7f837708cee3f68a3336356672fa63c80af8e3a
marcobrasci1986 <marco_brasci@hotmail.com>
2019-05-08 14:36:48 +0200

    init commit

---
+++ src/main/java/io/axoniq/demo/giftcard/command/GiftCard.java
@@ -0,0 +1,77 @@
+package io.axoniq.demo.giftcard.command;
+
+import io.axoniq.demo.giftcard.api.*;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+
+import java.lang.invoke.MethodHandles;
+
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
+
+@Aggregate
+@Profile("command")
+public class GiftCard {
+
+    private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @AggregateIdentifier
+    private String id;
+    private int remainingValue;
+
+    public GiftCard() {
+        log.debug("empty constructor invoked");
+    }
+
+    /**
+     * The Command Handlers should decide whether the Aggregate is in the correct state. If yes, an Event is published.
+     * If not, the Command might be ignored or an exception could be thrown, depending on the needs of the domain.
+     * @param cmd
+     */
+    @CommandHandler
+    public GiftCard(IssueCmd cmd) {
+        log.debug("handling {}", cmd);
+        if(cmd.getAmount() <= 0) throw new IllegalArgumentException("amount <= 0");
+        apply(new IssuedEvt(cmd.getId(), cmd.getAmount()));
+    }
+
+    @CommandHandler
+    public void handle(RedeemCmd cmd) {
+        log.debug("handling {}", cmd);
+        if(cmd.getAmount() <= 0) throw new IllegalArgumentException("amount <= 0");
+        if(cmd.getAmount() > remainingValue) throw new IllegalStateException("amount > remaining value");
+        apply(new RedeemedEvt(id, cmd.getAmount()));
+    }
+
+    @CommandHandler
+    public void handle(CancelCmd cmd) {
+        log.debug("handling {}", cmd);
+        apply(new CancelEvt(id));
+    }
+
+    @EventSourcingHandler
+    public void on(IssuedEvt evt) {
+        log.debug("applying {}", evt);
+        id = evt.getId();
+        remainingValue = evt.getAmount();
+        log.debug("new remaining value: {}", remainingValue);
+    }
+
+    @EventSourcingHandler
+    public void on(RedeemedEvt evt) {
+        log.debug("applying {}", evt);
+        remainingValue -= evt.getAmount();
+        log.debug("new remaining value: {}", remainingValue);
+    }
+
+    @EventSourcingHandler
+    public void on(CancelEvt evt) {
+        log.debug("applying {}", evt);
+        remainingValue = 0;
+        log.debug("new remaining value: {}", remainingValue);
+    }
+}
